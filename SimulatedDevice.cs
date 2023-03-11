f08d3fb62d8c5584db906d2c260afe4bf357abc1
Indrajeet Singh <indrajeetsinghmaan@gmail.com>
2023-03-11 20:56:15 +0530

    added devicePropertiesChanges callback

--- SimulatedDevice.cs
+++ SimulatedDevice.cs
@@ -18,42 +18,42 @@ namespace simulatedDevice
         private static DeviceClient s_deviceClient;
 
         // The device connection string to authenticate the device with your IoT hub.
-        private const string s_connectionString = "connectionstring";
-        private const string s_serviceConnectionString = "connection string with service access policy";
+        private const string s_connectionString = "HostName=ioth.azure-devices.net;DeviceId=iot-dev1;SharedAccessKey=1MQy4Ao99NXQ7oWVPT1l8zA19a39ZLGDj0pmI6K5TnU=";
+        private const string s_serviceConnectionString = "HostName=ioth.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=tOqSnXV1zygNGAHzAaTSEH/dq3nkWWZmwbSPhesIBO4=";
 
         // Async method to send simulated telemetry
-        // private static async void SendDeviceToCloudMessagesAsync()
-        // {
-        //     // Initial telemetry values
-        //     double minTemperature = 20;
-        //     double minHumidity = 60;
-        //     Random rand = new Random();
+        private static async void SendDeviceToCloudMessagesAsync()
+        {
+            // Initial telemetry values
+            double minTemperature = 20;
+            double minHumidity = 60;
+            Random rand = new Random();
 
-        //     while (true)
-        //     {
-        //         double currentTemperature = minTemperature + rand.NextDouble() * 15;
-        //         double currentHumidity = minHumidity + rand.NextDouble() * 20;
+            while (true)
+            {
+                double currentTemperature = minTemperature + rand.NextDouble() * 15;
+                double currentHumidity = minHumidity + rand.NextDouble() * 20;
 
-        //         // Create JSON message
-        //         var telemetryDataPoint = new
-        //         {
-        //             temperature = currentTemperature,
-        //             humidity = currentHumidity
-        //         };
-        //         var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
-        //         var message = new Microsoft.Azure.Devices.Client.Message(Encoding.ASCII.GetBytes(messageString));
+                // Create JSON message
+                var telemetryDataPoint = new
+                {
+                    temperature = currentTemperature,
+                    humidity = currentHumidity
+                };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Microsoft.Azure.Devices.Client.Message(Encoding.ASCII.GetBytes(messageString));
 
-        //         // Add a custom application property to the message.
-        //         // An IoT hub can filter on these properties without access to the message body.
-        //         message.Properties.Add("temperatureAlert", (currentTemperature > 30) ? "true" : "false");
+                // Add a custom application property to the message.
+                // An IoT hub can filter on these properties without access to the message body.
+                message.Properties.Add("temperatureAlert", (currentTemperature > 30) ? "true" : "false");
 
-        //         // Send the tlemetry message
-        //         await s_deviceClient.SendEventAsync(message).ConfigureAwait(false);
-        //         Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+                // Send the tlemetry message
+                await s_deviceClient.SendEventAsync(message).ConfigureAwait(false);
+                Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
 
-        //         await Task.Delay(1000).ConfigureAwait(false);
-        //     }
-        // }
+                await Task.Delay(1000).ConfigureAwait(false);
+            }
+        }
 
 
         private static async void ReceiveC2dAsync()
@@ -76,6 +76,18 @@ namespace simulatedDevice
             var commandMessage = new Microsoft.Azure.Devices.Message(Encoding.ASCII.GetBytes("This is my c2d message"));
             await serviceClient.SendAsync(targetDevice, commandMessage);
         }
+
+
+        static async void HandleDesiredPropertiesChange()
+        {
+            await s_deviceClient.SetDesiredPropertyUpdateCallbackAsync(async (desired,ctx) => 
+            {
+                Newtonsoft.Json.Linq.JValue fpsJson = desired["FPS"];
+                var fps = fpsJson.Value;
+
+                Console.WriteLine("Received desired FPS: {0}", fps);
+            },null);
+        }
         private static void Main()
         {
             Console.WriteLine("IoT Hub Quickstarts - Simulated device. Ctrl-C to exit.\n");
@@ -84,7 +96,8 @@ namespace simulatedDevice
             s_deviceClient = DeviceClient.CreateFromConnectionString(s_connectionString, Microsoft.Azure.Devices.Client.TransportType.Mqtt);
             // SendDeviceToCloudMessagesAsync();
             // ReceiveC2dAsync();
-            SendCloudToDeviceMessageAsync();
+            HandleDesiredPropertiesChange();
+            // SendCloudToDeviceMessageAsync();
             Console.ReadLine();
         }
     }
